78f0ec0364d090910e0756e4bd836f84d19ab846
Ahmed Zermane <ahmed.zermane@lirmm.fr>
2023-07-04 09:28:18 +0200

    minor

--- src/Go1.in.cpp
+++ src/Go1.in.cpp
@@ -9,20 +9,20 @@ namespace
 {
 
 // This is set by CMake, see CMakeLists.txt
-static const std::string JVRC_DESCRIPTION_PATH = "@JVRC_DESCRIPTION_PATH@";
+static const std::string GO1_DESCRIPTION_PATH = "@GO1_DESCRIPTION_PATH@";
 
 } // namespace
 
 namespace mc_robots
 {
 
-Go1::Go1() : mc_rbdyn::RobotModule(JVRC_DESCRIPTION_PATH, "jvrc1")
+Go1::Go1() : mc_rbdyn::RobotModule(GO1_DESCRIPTION_PATH, "go1")
 {
   // True if the robot has a fixed base, false otherwise
   bool fixed = false;
   // Makes all the basic initialization that can be done from an URDF file
   init(rbd::parsers::from_urdf_file(urdf_path, fixed));
-
+  rsdf_dir = path + "/rsdf/" + name + "/";
   // Automatically load the convex hulls associated to each body
   std::string convexPath = path + "/convex/" + name + "/";
   bfs::path p(convexPath);
@@ -41,50 +41,72 @@ Go1::Go1() : mc_rbdyn::RobotModule(JVRC_DESCRIPTION_PATH, "jvrc1")
       }
     }
   }
+  mc_rtc::log::success("PandaRobotModule uses urdf_path {}", urdf_path);
+  mc_rtc::log::success("PandaRobotModule uses convexPath {}", convexPath);
+  mc_rtc::log::success("PandaRobotModule uses rsdf_dir {}", rsdf_dir);
+    //Ref joint order
+  _ref_joint_order = {"FR_hip_joint", "FR_thigh_joint", "FR_calf_joint",
+		      "FL_hip_joint", "FL_thigh_joint", "FL_calf_joint",
+		      "RR_hip_joint", "RR_thigh_joint", "RR_calf_joint",
+		      "RL_hip_joint", "RL_thigh_joint", "RL_calf_joint"
+  };
+  // Stance: joint name, angle in degrees
+    // // Default joint configuration, if a joint is omitted the configuration is 0 or the middle point of the limit range if
+  // // 0 is not a valid configuration
+  std::map<std::string, double> standing
+  {
+    {"FR_hip_joint", 0.0},
+    {"FR_thigh_joint", 0.67},
+    {"FR_calf_joint", -1.3},
+    {"FL_hip_joint", 0.0},
+    {"FL_thigh_joint", 0.67},
+    {"FL_calf_joint", -1.3},
+    {"RR_hip_joint", 0.0},
+    {"RR_thigh_joint", 0.67},
+    {"RR_calf_joint", -1.3},
+    {"RL_hip_joint", 0.0},
+    {"RL_thigh_joint", 0.67},
+    {"RL_calf_joint", -1.3},
+  };
 
-  // Define some force sensors
-  _forceSensors.push_back(mc_rbdyn::ForceSensor("RightFootForceSensor", "R_ANKLE_P_S", sva::PTransformd::Identity()));
-  _forceSensors.push_back(mc_rbdyn::ForceSensor("LeftFootForceSensor", "L_ANKLE_P_S", sva::PTransformd::Identity()));
-  _forceSensors.push_back(mc_rbdyn::ForceSensor("RightHandForceSensor", "R_WRIST_Y_S", sva::PTransformd::Identity()));
-  _forceSensors.push_back(mc_rbdyn::ForceSensor("LeftHandForceSensor", "L_WRIST_Y_S", sva::PTransformd::Identity()));
+    for(const auto & j : mb.joints())
+  {
+    if(j.name() != "Root" && j.dof() > 0)
+    {
+      _stance[j.name()] = {standing.at(j.name())};
+    }
+  }
+    // // Default configuration of the floating base
+  _default_attitude = {1., 0., 0., 0., 0., 0., 0.35};
 
-  // Define body sensors
-  _bodySensors.emplace_back("Accelerometer", "PELVIS_S", sva::PTransformd(Eigen::Vector3d(-0.0325, 0, 0.1095)));
-  _bodySensors.emplace_back("FloatingBase", "PELVIS_S", sva::PTransformd::Identity());
 
-  // Define a minimal set of self-collisions
+  _bodySensors.emplace_back("Accelerometer", "trunk", sva::PTransformd(Eigen::Vector3d(0., 0., 0.)));
+  _bodySensors.emplace_back("FloatingBase", "trunk", sva::PTransformd::Identity());
+   // // Define a minimal set of self-collisions
   _minimalSelfCollisions = {
-      {"WAIST_R_S", "L_SHOULDER_Y_S", 0.02, 0.001, 0.}, {"WAIST_R_S", "R_SHOULDER_Y_S", 0.02, 0.001, 0.},
-      {"PELVIS_S", "R_ELBOW_P_S", 0.05, 0.001, 0.},     {"PELVIS_S", "L_ELBOW_P_S", 0.05, 0.001, 0.},
-      {"R_WRIST_Y_S", "R_HIP_Y_S", 0.05, 0.025, 0.},    {"L_WRIST_Y_S", "L_HIP_Y_S", 0.05, 0.025, 0.}};
+    {"FR_hip", "trunk", 0.02, 0.01, 0.0},
+    {"FL_hip", "trunk", 0.02, 0.01, 0.0},
+    {"FR_calf","RR_thigh", 0.02, 0.01, 0.0},
+    {"FR_calf","RR_calf", 0.02, 0.01, 0.0},
+    {"FL_calf","RL_thigh", 0.02, 0.01, 0.0},
+    {"FL_calf","RL_calf", 0.02, 0.01, 0.0},
+    {"FR_calf","FL_calf", 0.02, 0.01, 0.0},
+    {"RR_calf","RL_calf", 0.02, 0.01, 0.0},
+    {"RR_hip", "trunk", 0.02, 0.01, 0.0},
+    {"RL_hip", "trunk", 0.02, 0.01, 0.0},
+    {"FR_thigh","FL_thigh", 0.02, 0.01, 0.0},
+    {"FR_thigh","FL_calf", 0.02, 0.01, 0.0},
+    {"FR_calf","FL_thigh", 0.02, 0.01, 0.0},
+    {"RR_thigh","RL_thigh", 0.02, 0.01, 0.0},
+    {"RR_thigh","RL_calf", 0.02, 0.01, 0.0},
+    {"RR_calf","RL_thigh", 0.02, 0.01, 0.0}
+
+  };
+
   _commonSelfCollisions = _minimalSelfCollisions;
 
-  // Define simple grippers
-  _grippers = {{"l_gripper", {"L_UTHUMB"}, true}, {"r_gripper", {"R_UTHUMB"}, false}};
 
-  // Default configuration of the floating base
-  _default_attitude = {{1., 0., 0., 0., 0., 0., 0.8275}};
 
-  // Default joint configuration, if a joint is omitted the configuration is 0 or the middle point of the limit range if
-  // 0 is not a valid configuration
-  _stance["L_ANKLE_P"] = {-0.33};
-  _stance["L_ANKLE_R"] = {-0.02};
-  _stance["L_ELBOW_P"] = {-0.52};
-  _stance["L_HIP_P"] = {-0.38};
-  _stance["L_HIP_R"] = {0.02};
-  _stance["L_KNEE"] = {0.72};
-  _stance["L_SHOULDER_P"] = {-0.052};
-  _stance["L_SHOULDER_R"] = {0.17};
-  _stance["L_SHOULDER_Y"] = {0.0};
-  _stance["R_ANKLE_P"] = {-0.33};
-  _stance["R_ANKLE_R"] = {-0.01};
-  _stance["R_ELBOW_P"] = {-0.52};
-  _stance["R_HIP_P"] = {-0.38};
-  _stance["R_HIP_R"] = {-0.01};
-  _stance["R_KNEE"] = {0.72};
-  _stance["R_SHOULDER_P"] = {-0.052};
-  _stance["R_SHOULDER_R"] = {-0.17};
-  _stance["WAIST_P"] = {0.13};
 }
 
 } // namespace mc_robots

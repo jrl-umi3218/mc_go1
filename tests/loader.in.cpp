57763dcbf672c573e17ec5529e5fe2980a6d80b6
Arnaud TANGUY <arn.tanguy@gmail.com>
2023-07-06 13:12:33 +0200

    Fix test and pre-commit

--- tests/loader.in.cpp
+++ tests/loader.in.cpp
@@ -5,6 +5,6 @@ int main(int argc, char * argv[])
   if(argc < 2) { return 1; }
   mc_rbdyn::RobotLoader::clear();
   mc_rbdyn::RobotLoader::update_robot_module_path({std::string(argv[1])});
-  auto rm = mc_rbdyn::RobotLoader::get_robot_module(@ROBOT_MODULE_PARAMS @);
+  auto rm = mc_rbdyn::RobotLoader::get_robot_module(@ROBOT_MODULE_PARAMS@);
   return 0;
 }

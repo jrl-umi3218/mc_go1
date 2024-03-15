349376336640d9229a0ab2fe9bbdd5eb2a1af336
Arnaud TANGUY <arn.tanguy@gmail.com>
2024-03-15 15:02:39 +0100

    [cmake] Support ROS2/ROS1

--- src/config.in.h
+++ src/config.in.h
@@ -6,6 +6,6 @@ namespace mc_rtc
 {
 
 // This is set by CMake, see CMakeLists.txt
-static const std::string GO1_DESCRIPTION_PATH = "@GO1_DESCRIPTION_PATH@";
+static const std::string GO1_DESCRIPTION_PATH = "@MC_GO1_DESCRIPTION_PATH@";
 
 } // namespace mc_rtc
--- tests/loader.in.cpp
+++ tests/loader.in.cpp
@@ -1,12 +1,13 @@
 #include <mc_rbdyn/RobotLoader.h>
+#include <iostream>
 
 int main(int argc, char * argv[])
 {
-  if(argc < 2) { return 1; }
   mc_rbdyn::RobotLoader::clear();
   mc_rbdyn::RobotLoader::update_robot_module_path({std::string(argv[1])});
   // clang-format off
   auto rm = mc_rbdyn::RobotLoader::get_robot_module(@ROBOT_MODULE_PARAMS@);
+  std::cout << "rm " << std::endl;
   // clang-format on
   return 0;
 }
